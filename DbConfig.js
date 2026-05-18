8108b60061e6b6f19370ec41bf71b3e4738659bc
AmirSoltani2002 <amir.soltani1381@gmail.com>
2026-05-18 13:56:55 +0200

    fix: config hidden

--- DbConfig.js
+++ DbConfig.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : 'expense',
-    DB_PWD : 'ExpenseApp123!',
-    DB_DATABASE : 'expensedb'
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PWD: process.env.DB_PWD,
+    DB_DATABASE: process.env.DB_DATABASE
 });
